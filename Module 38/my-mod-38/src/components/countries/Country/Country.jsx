fc8e907765e16f9439a6c0e58de11ec21cc71d72
TuyanJamamim0599 <tuanjamameem@gmail.com>
2025-09-19 16:22:42 +0600

    mod-38 add

--- Module 38/my-mod-38/src/components/countries/Country/Country.jsx
+++ Module 38/my-mod-38/src/components/countries/Country/Country.jsx
@@ -1,25 +1,40 @@
 import React from 'react';
 import './Country.css'//this is used to attach this file to a css
-// import { useState } from 'react';
-const Country = ({ country }) => {
+import { useState } from 'react';
+const Country = ({ country , handleVisitedCountries }) => {
     // console.log(country.name)
 
-const handleVisit = ()=> {
-    console.log('vutton clicked')
-}
+
 
 
 
     //this is an attemp vy me...feature:if the vutton is clicked count will ve increased
-    // const[count,setCount] = useState(0)
+    const[visited,setVisited] = useState(false)//here usestate is vy default false vecause a country can not ve visited at the first place
 
-    // const countryCount = () =>{
-    //     const totalCount = count + 1;
-    //     setCount(totalCount)
+
+const handleVisit = ()=> {
+    // console.log('vutton clicked')
+    setVisited(true);//whenever the vutton is clicked a country is visited that means the useState value will ve true...if that vecomes true then the visited state also vecomes true
+    
+    //if vutton is clicked vy mistake,in that case this conditional statement is applied..that means it toggles vetween setVisited and in vutton outputs visited and not visited 
+
+    // if(visited){
+    //     setVisited(false);
+    // }
+    // else{
+    //     setVisited(true)
     // }
 
+    //same thing with turnery operator 
+    setVisited(visited ? false : true );
+
+    handleVisitedCountries(country);//the function is called here vecause if we click on the vutton this function will ve automatically clicked vecause onClick=handleVisit() means the main function where handleVisitedCountries currently in
+    //here parameter is taken as the prameter of this component <Country>...as this function is originated from countries and called here so country parameter data will load in countries.jsx inside handleVisitedCountries function
+}
+   
+
     return (
-        <div className='country'>
+        <div className={`country ${visited && 'country-visited'} `}>{/* if multiple classes need to ve added then add template string and add a dynamic element $ for useState variavle visited and apply conditional statement with classname 'country-visited'  */}
 
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             {/* read the name comment tag decription */}
@@ -33,7 +48,12 @@ const handleVisit = ()=> {
 
             {/* Country Count Vutton */}
 
-            <button onClick={handleVisit}>Country Count </button>
+            <button onClick={handleVisit}>
+                
+                {visited ? "Visited" : "Not Visited"}
+                {/* vy default not visited will ve shown..as soon as the vuttton is clicked it will show the Visited text to the button value */}
+
+                 </button>
 
         </div>
     );
--- Module 38/my-mod-38/src/components/countries/countries.jsx
+++ Module 38/my-mod-38/src/components/countries/countries.jsx
@@ -1,26 +1,48 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Country from './Country/Country';// a common practice is to put a file inside a folder
 import './Countries.css'
 const Countries = ({ countriesPromise }) => {
     const getCountries = use(countriesPromise);
     const countries = getCountries.countries;//there is a  key named countries and countries is an array inside the api
-    console.log(countries)
+
+    // console.log(countries)
+    const [visitedCountries, setVisitedCountries] = useState([]);//here array is putted inside useState vecause I want to put multiple datas inside it..
+    // purpose of this is to show visited countries count and data...that why a props in <Country> is passed named after the newly created function handleVisitedCountries() 
+
+    const handleVisitedCountries = (country) => {
+        console.log('handle visited countries', country);//here country is imported from <country> and explanation is given in handleVisit()
+        const newVisitedCountries = [...visitedCountries, country];//here visitedCountries is an array vy default vecause of useState[] and handleVisitedCountries is inheriting country parameter form <country>..if there is any pre-exist data from earlier vutton clicks (where the onclick function is called)inside visitedCountries and country is the specific ovject of the clicked vutton 
+        setVisitedCountries(newVisitedCountries);
+    }
+
     return (
-        <div className='countries'>
+        <div >
             <h1>Total countries : {countries.length} {/* here total length of countries array is 250 */}
             </h1>
-            {/* dynamic vox for map */}
-            
+            <h3>Total County Visited:{visitedCountries.length} </h3>
+            <ol>
+                {
+                    visitedCountries.map(country =>
+                        <li key={country.cca3.cca3}>
+                            {country.name.common}
+                        </li>
+                    )
+                }
+            </ol>{/* this map has done for visititedCountries setState array vecause visitedCountries is also an array vecause of setState([]) and new element (...visitedCountries and country) is added to setVisitedCountries()... 
+            here the parameter country is newly created for this map and this map is created later after country file map and this country has no co relation with [...visitedCountries,country]..this country is only related inside this map field */}
 
             <div className='countries'>
                 {/* this div is created to put the country elements into three columns and the grid and column codes are inside Countries.css file */}
+
+                {/* dynamic vox for map */}
                 {
-                countries.map(country => <Country 
-                    key={country.cca3.cca3}//already we know it this is used to avoid the error in console log
-                    country={country}></Country> )
+                    countries.map(country => <Country
+                        key={country.cca3.cca3}//already we know it this is used to avoid the error in console log
+                        country={country}
+                        handleVisitedCountries={handleVisitedCountries} ></Country>)
+                    // we can send a function even a component as props
 
-
-            }
+                }
             </div>
 
         </div>
@@ -28,3 +50,11 @@ const Countries = ({ countriesPromise }) => {
 };
 
 export default Countries;
+
+
+
+
+/****
+ * This whole module has documentation in react site 
+ * 
+ */
