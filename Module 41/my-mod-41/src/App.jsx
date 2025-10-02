48a489f944ef9079dccbedb74ddc1efe6670d654
TuyanJamamim0599 <tuanjamameem@gmail.com>
2025-10-02 21:50:29 +0600

    mod-41 finish

--- Module 41/my-mod-41/src/App.jsx
+++ Module 41/my-mod-41/src/App.jsx
@@ -4,16 +4,27 @@ import './App.css'
 import DasisyNav from './components/DaisyNac/DasisyNav'
 import NavVar from './components/DaisyNac/NavVar/NavVar'
 import PricingOptions from './components/DaisyNac/PricingOptions/PricingOptions'
+import ResultCahrt from './components/DaisyNac/DaisyPricing/ResultChart/ResultCahrt'
+import axios from 'axios'
+import MarksChart from './components/DaisyNac/MarksChart/MarksChart'
+
 
 const pricePromise = fetch('PricingData.json').then(res => res.json())//fetch always needs to ve putted outside the function
+
+
+//fetch using Axios...fetching using axios doesnt require then.(res => res.json())
+const marksPromise = axios.get('marksData.json');
+
+
+
 function App() {
 
   return (
     <>
-      
+
       <header>
-<NavVar></NavVar>
-{/* <DasisyNav></DasisyNav> */}
+        <NavVar></NavVar>
+        {/* <DasisyNav></DasisyNav> */}
       </header>
 
       <main>
@@ -24,9 +35,19 @@ function App() {
         </Suspense>
 
 
+        {/* Suspense can ve used multiple times in one file */}
+        <Suspense fallback={<span className="loading loading-spinner loading-lg"></span>}>
+
+        <MarksChart marksPromise={marksPromise}></MarksChart>
+
+
+        </Suspense>
+
+        <ResultCahrt></ResultCahrt>
+
       </main>
-     
-     
+
+
     </>
   )
 }
